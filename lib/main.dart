0e8a0bfe97200a4822ee1ad20c6ccc02d8cd0193
Alfred Muinde Tuva <tuva@ke-atuva-mac.local>
2025-02-05 13:54:40 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:rpg/screens/home/home.dart';
+import 'package:rpg/theme.dart';
+
+void main() {
+  runApp(MaterialApp(theme: primaryTheme, home: Home()));
+}
+
+class Sandbox extends StatelessWidget {
+  const Sandbox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sandbox'),
+        backgroundColor: Colors.grey,
+      ),
+      body: Text("Sandbox"),
+    );
+  }
+}
---
+++ lib/screens/home/home.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:rpg/shared/styled_text.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: StyledTitle("Your Characters"),
+        centerTitle: true,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          children: [
+            StyledText("Character list:"),
+            StyledHeading("Character list:"),
+            StyledTitle("Character list:")
+          ],
+        ),
+      ),
+    );
+  }
+}
