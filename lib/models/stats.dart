154f7d87234d4a61a8543c5026049c382cbbb2c8
Alfred Tuva <muindetuva@gmail.com>
2025-02-07 12:26:51 +0300

    Add character class and stats mixin

---
+++ lib/models/stats.dart
@@ -0,0 +1,45 @@
+mixin Stats {
+  int _points = 10;
+  int _health = 10;
+  int _attack = 10;
+  int _defense = 10;
+  int _skill = 10;
+
+  void increaseStats(String stat) {
+    if (_points > 0) {
+      if (stat == 'health') {
+        _health++;
+      }
+
+      if (stat == 'health') {
+        _attack++;
+      }
+      if (stat == 'health') {
+        _defense++;
+      }
+      if (stat == 'health') {
+        _skill++;
+      }
+      _points--;
+    }
+  }
+
+  void decreaseStats(String stat) {
+    if (stat == 'health' && _health > 5) {
+      _health--;
+      _points++;
+    }
+    if (stat == 'attack' && _attack > 5) {
+      _attack--;
+      _points++;
+    }
+    if (stat == 'defense' && _defense > 5) {
+      _defense--;
+      _points++;
+    }
+    if (stat == 'skill' && _skill > 5) {
+      _skill--;
+      _points++;
+    }
+  }
+}
