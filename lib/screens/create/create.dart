76f1cf1c5839f6b74c447a9f1e2372e0a9bcba38
Alfred Tuva <muindetuva@gmail.com>
2025-02-07 17:07:38 +0300

    Add scrollview widget

--- lib/screens/create/create.dart
+++ lib/screens/create/create.dart
@@ -50,75 +50,78 @@ class _CreateState extends State<Create> {
         ),
         body: Container(
           padding: EdgeInsets.symmetric(vertical: 30, horizontal: 20),
-          child: Column(
-            children: [
-              // Welcome message
-              Center(
-                child: Icon(
-                  Icons.code,
-                  color: AppColors.primaryColor,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                // Welcome message
+                Center(
+                  child: Icon(
+                    Icons.code,
+                    color: AppColors.primaryColor,
+                  ),
                 ),
-              ),
-              Center(
-                child: StyledHeading("Welcome New Player"),
-              ),
-              Center(
-                child: StyledText("Create a name & slogan fro your character"),
-              ),
-              SizedBox(height: 30),
+                Center(
+                  child: StyledHeading("Welcome New Player"),
+                ),
+                Center(
+                  child:
+                      StyledText("Create a name & slogan fro your character"),
+                ),
+                SizedBox(height: 30),
 
-              // Input for name and slogan
-              TextField(
-                controller: _nameController,
-                decoration: InputDecoration(
-                  prefixIcon: Icon(Icons.person_2),
-                  label: StyledText("Character name"),
+                // Input for name and slogan
+                TextField(
+                  controller: _nameController,
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.person_2),
+                    label: StyledText("Character name"),
+                  ),
+                  style: GoogleFonts.kanit(
+                    textStyle: Theme.of(context).textTheme.bodyMedium,
+                  ),
+                  cursorColor: AppColors.textColor,
                 ),
-                style: GoogleFonts.kanit(
-                  textStyle: Theme.of(context).textTheme.bodyMedium,
+                SizedBox(height: 20),
+                TextField(
+                  controller: _sloganController,
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.chat),
+                    label: StyledText("Character slogan"),
+                  ),
+                  style: GoogleFonts.kanit(
+                    textStyle: Theme.of(context).textTheme.bodyMedium,
+                  ),
+                  cursorColor: AppColors.textColor,
                 ),
-                cursorColor: AppColors.textColor,
-              ),
-              SizedBox(height: 20),
-              TextField(
-                controller: _sloganController,
-                decoration: InputDecoration(
-                  prefixIcon: Icon(Icons.chat),
-                  label: StyledText("Character slogan"),
+                SizedBox(height: 30),
+                // Select vocation title
+                Center(
+                  child: Icon(
+                    Icons.code,
+                    color: AppColors.primaryColor,
+                  ),
                 ),
-                style: GoogleFonts.kanit(
-                  textStyle: Theme.of(context).textTheme.bodyMedium,
+                Center(
+                  child: StyledHeading("Choose a vocation"),
                 ),
-                cursorColor: AppColors.textColor,
-              ),
-              SizedBox(height: 30),
-              // Select vocation title
-              Center(
-                child: Icon(
-                  Icons.code,
-                  color: AppColors.primaryColor,
+                Center(
+                  child: StyledText("This determines your available skills"),
                 ),
-              ),
-              Center(
-                child: StyledHeading("Choose a vocation"),
-              ),
-              Center(
-                child: StyledText("This determines your available skills"),
-              ),
-              SizedBox(height: 30),
+                SizedBox(height: 30),
 
-              // vocation cards
-              VocationCard(vocation: Vocation.junkie),
-              VocationCard(vocation: Vocation.ninja),
-              VocationCard(vocation: Vocation.raider),
-              VocationCard(vocation: Vocation.wizard),
+                // vocation cards
+                VocationCard(vocation: Vocation.junkie),
+                VocationCard(vocation: Vocation.ninja),
+                VocationCard(vocation: Vocation.raider),
+                VocationCard(vocation: Vocation.wizard),
 
-              Center(
-                child: StyledButton(
-                    onPressed: handleSubmit,
-                    child: StyledHeading("Create Character")),
-              ),
-            ],
+                Center(
+                  child: StyledButton(
+                      onPressed: handleSubmit,
+                      child: StyledHeading("Create Character")),
+                ),
+              ],
+            ),
           ),
         ));
   }
