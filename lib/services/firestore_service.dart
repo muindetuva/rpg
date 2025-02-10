43ba4e41971363faef9123f372d11b1548560162
Alfred Tuva <muindetuva@gmail.com>
2025-02-10 23:28:37 +0300

    Use converters on ref

--- lib/services/firestore_service.dart
+++ lib/services/firestore_service.dart
@@ -1,5 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:rpg/models/character.dart';
 
 class FirestoreService {
-  static final ref = FirebaseFirestore.instance.collection('characters');
+  static final ref = FirebaseFirestore.instance
+      .collection('characters')
+      .withConverter(
+          fromFirestore: Character.fromFirestore,
+          toFirestore: (Character c, _) => c.toFirestore());
 }
